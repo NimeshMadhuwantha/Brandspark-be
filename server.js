0def229418ccd1b07db170e9bf7941bbc8e64964
nethhari <chemininethhari@gmail.com>
2024-06-21 14:53:56 +0530

    Created mongodb connection

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 5000;
+const URL = "mongodb+srv://nethhari:Chemma%40mongodb@cluster0.6co1avu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 app.use(bodyParser.json());
+
+mongoose.connect(URL)
+    .then(() => {
+        console.log("Connected")
+    })
+    .catch((e) => {
+        console.log("DB error", e)
+    })
 
 app.listen(PORT, () => console.log("Server is up"));
