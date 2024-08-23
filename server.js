3ca061bd062086703d3acae75842dcea137fa3cc
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-08-23 14:30:27 +0530

    Backend update

--- server.js
+++ server.js
@@ -1,55 +1,22 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import bcrypt from 'bcrypt';
-import Customer from './models/Customer.js'; // Adjust the path as necessary
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes.mjs');
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
-const PORT = 5000;
-const URL = "mongodb+srv://nethhari:Chemma%40mongodb@cluster0.6co1avu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((e) => {
-        console.log("DB error", e);
-    });
+app.use('/api/users', userRoutes);
 
-app.post('/api/signup', async (req, res) => {
-    const { firstName, secondName, email, password } = req.body;
-    
-    try {
-        const newCustomer = new Customer({ firstName, secondName, email, password });
-        await newCustomer.save();
-        res.status(201).send({ message: 'User signed up successfully' });
-    } catch (error) {
-        res.status(400).send({ error: error.message });
-    }
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
-
-app.post('/api/login', async (req, res) => {
-    const { email, password } = req.body;
-
-    try {
-        const customer = await Customer.findOne({ email });
-        if (!customer) {
-            return res.status(401).send({ message: 'Invalid email or password' });
-        }
-
-        const isMatch = await bcrypt.compare(password, customer.password);
-        if (!isMatch) {
-            return res.status(401).send({ message: 'Invalid email or password' });
-        }
-
-        res.status(200).send({ message: 'User logged in successfully' });
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-    }
-});
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
